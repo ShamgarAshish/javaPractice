6f0e5fc2dd600da3d095f6eff88bc6296599c7b6
shamgar <shamgarashish@gmail.com>
2016-08-25 21:29:15 -0700

    check division by zero zero

--- src/main/java/javaPractice/Car.java
+++ src/main/java/javaPractice/Car.java
@@ -4,6 +4,7 @@ public class Car {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(10/0);
 System.out.println("hello car");
 System.out.println("commit check");
 	}
