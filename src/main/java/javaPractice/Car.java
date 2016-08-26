9658f087f77a49be8bee5871884b9d190cca42b0
shamgar <shamgarashish@gmail.com>
2016-08-25 20:54:52 -0700

    add multibranch check code

--- src/main/java/javaPractice/Car.java
+++ src/main/java/javaPractice/Car.java
@@ -1,13 +1,13 @@
 package javaPractice;
-
 public class Car {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello car");
 System.out.println("commit check");
 System.out.println("branch first");
 System.out.println("new Branch second");
+int i=0;
+System.out.println(i++);
+System.out.println("multibranch auto ");
 	}
-
 }
