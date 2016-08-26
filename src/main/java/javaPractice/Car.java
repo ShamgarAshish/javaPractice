6322dc24f4efbb563750c51d2a05bad647383c55
shamgar <shamgarashish@gmail.com>
2016-08-25 22:06:18 -0700

    division by zero

--- src/main/java/javaPractice/Car.java
+++ src/main/java/javaPractice/Car.java
@@ -4,7 +4,7 @@ public class Car {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(10);
+		System.out.println(10/0);
 System.out.println("hello car");
 System.out.println("commit check");
 System.out.println("branch first");
