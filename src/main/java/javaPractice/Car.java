4288dee8817953b11bb275c694a9afc1afb8dc04
shamgar <shamgarashish@gmail.com>
2016-08-29 03:23:49 -0700

    final checkk

--- src/main/java/javaPractice/Car.java
+++ src/main/java/javaPractice/Car.java
@@ -11,7 +11,7 @@ System.out.println("branch first");
 System.out.println("checkAutoBuildmaster");
 System.out.println("empty");
 System.out.println("check git hook post commit");
-System.out.println("final check");
+System.out.println("final check1");
 	}
 
 }
