3ad4e3e5bed8752540191b258be34abcf88d2975
shamgar <shamgarashish@gmail.com>
2016-08-29 02:23:28 -0700

    second check

--- src/main/java/javaPractice/Car.java
+++ src/main/java/javaPractice/Car.java
@@ -11,6 +11,7 @@ System.out.println("branch first");
 System.out.println("checkAutoBuildmaster");
 System.out.println("empty");
 System.out.println("check git hook post commit");
+System.out.println();
 	}
 
 }
